6ab81550b0268c6f0e07f9ea9aa75fee5191ac80
ragaaviatrix <90199568+ragaaviatrix@users.noreply.github.com>
2022-01-18 15:51:47 +0000

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,7 @@
 terraform {
   required_version = ">= 1.0.2"
   backend "remote" {
+    hostname     = "app.terraform.io"
     organization = "aviatrix-mcn"
     workspaces {
       name = "ace-iac-day-two"
